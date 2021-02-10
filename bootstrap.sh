a6aa81b978b94ef52ab4627611a0609426ac5ee3
Ethan Rose <errose28@gmail.com>
2021-02-10 02:34:54 +0000

    Add wheel and setuptools for installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 # Installs libraries for dotfiles deployment.
 python -m pip install \
     setuptools \
+    wheel \
+    robotframework \
     git+https://github.com/errose28/DotfilesLibrary.git \
     git+https://github.com/errose28/DotfilesLibraryListeners.git
 
