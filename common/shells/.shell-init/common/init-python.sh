ffd8b07ff1262c2e68203bf18f234c0640b44801
Ethan Rose <errose28@gmail.com>
2021-02-12 13:25:02 -0500

    Add notes to python init script

--- common/shells/.shell-init/common/init-python.sh
+++ common/shells/.shell-init/common/init-python.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
-# Script to set up pyenv virtualenv if installed.
+
+# Script to init python environment optionally using pyenv and virtualenv.
+
+# pyenv will be enabled if the pyenv command is found in the PATH.
+# pyenv version will be autoamtically set to the PYENV_VERSION variable if set.
+# This version will be installed if needed (TODO).
+
+# virtualenv will be enabled if virtualenvwrapper.sh is found in the path or
+# pyenv prefix directory.
+# If DEFAULT_VIRTUALENV environment variable is set, this will be the virtual environment enabled.
+# It will be created if it does not exist.
 
 # Default path to virtualenvwrapper init script if not using pyenv.
 venv_wrapper_path="$(which virtualenvwrapper.sh)"
@@ -10,11 +20,11 @@ if command -v pyenv 1>/dev/null 2>&1; then
     venv_wrapper_path="$(pyenv prefix)"/bin/virtualenvwrapper.sh
 fi
 
-# Init virtualenvwrapper if used and use created env 'default' if it exists.
+# Init virtualenvwrapper if used.
 if [ "$venv_wrapper_path" ] && [ -f "$venv_wrapper_path" ]; then
     . "$venv_wrapper_path"
 
-    # Set this variable to use a default virtualenv in every shell.
+    # Create/set default virtualenv if specified.
     if [ "$DEFAULT_VIRTUALENV" ]; then
         if workon | grep -q "$DEFAULT_VIRTUALENV"; then
             workon "$DEFAULT_VIRTUALENV"
