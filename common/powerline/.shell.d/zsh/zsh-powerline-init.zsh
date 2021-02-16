f295aed252ddb1e39ee73f2db278280e3ceef59d
Ethan Rose <erose@apache.org>
2021-02-15 21:17:08 -0500

    Remove error message if powerline not installed with pip

--- common/powerline/.shell.d/zsh/zsh-powerline-init.zsh
+++ common/powerline/.shell.d/zsh/zsh-powerline-init.zsh
@@ -2,7 +2,7 @@
 
 # Find the powerline script and source it.
 # Script location will depend on the package manager used.
-pip_powerline_dir="$(pip show powerline-status | grep 'Location:' | awk '{print $2}')"
+pip_powerline_dir="$(pip show powerline-status 2>/dev/null | grep 'Location:' | awk '{print $2}')"
 powerline_dirs=(~/.nix-profile/share /usr/share/powerline/bindings "$pip_powerline_dir"/powerline/bindings)
 
 for pl_dir in $powerline_dirs; do
