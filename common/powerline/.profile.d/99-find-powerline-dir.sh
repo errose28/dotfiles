014e8744d9f1c2286c0201925a55e75e1d1bb9d8
Ethan Rose <erose@apache.org>
2021-02-27 13:09:07 -0500

    Update dir search order and add missing quotes

--- common/powerline/.profile.d/99-find-powerline-dir.sh
+++ common/powerline/.profile.d/99-find-powerline-dir.sh
@@ -5,8 +5,8 @@
 
 . ~/.shell.d/common/init-python.sh
 # Used if powerline installed with python -m pip.
-pip_powerline_dir=$(python -m site --user-site)
-powerline_dirs=""$HOME/.nix-profile/share" '/usr/share/powerline/bindings' "$pip_powerline_dir/powerline/bindings""
+pip_powerline_dir="$(python -m site --user-site)"
+powerline_dirs=""$pip_powerline_dir/powerline/bindings" "$HOME/.nix-profile/share" '/usr/share/powerline/bindings'"
 
 for pl_dir in $powerline_dirs; do
     if [ -d "$pl_dir" ]; then
