c845817619ed8642fd92adc5cd825cf95a0e0ff8
Ethan Rose <erose@apache.org>
2021-03-13 20:57:15 -0500

    Tmux git format v2: Add separator

---
+++ common/tmux/bin/is-git-dir.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Displays the first argument if in a git repo.
+# Otherwise, displays the secodnd argument if it is given.
+
+git_on="$1"
+git_off="$2"
+
+if git rev-parse --git-dir 2>/dev/null 1>/dev/null; then
+    echo "$git_on"
+elif [ -n "$git_off" ]; then
+    echo "$git_off"
+fi
--- common/tmux/bin/tmux-git-format.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-config="$1"
-if git rev-parse --git-dir 2>/dev/null 1>/dev/null; then
-    #tmux display "Called with $config from $(pwd)"
-    echo "$config"
-fi
