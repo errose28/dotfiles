8bb631e3d33eaf50777d37bf589d5b01008c7abb
Ethan Rose <erose@apache.org>
2021-05-16 15:47:39 -0400

    Update notes for nix polybar overlay

--- common/nixpkgs/.config/nixpkgs/overlays/polybar-overlay.nix
+++ common/nixpkgs/.config/nixpkgs/overlays/polybar-overlay.nix
@@ -1,5 +1,6 @@
 # Polybar has support for most extra features turned off by default in nixpkgs.
 # Turn them all on here.
+# Install with `nix-env -iA nixos.polybar-extra-support`
 self: super:
 {
     polybar-extra-support = super.polybar.override {
