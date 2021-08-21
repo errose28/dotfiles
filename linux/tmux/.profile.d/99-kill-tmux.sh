eb4335b1a1035a6b6007b98b843271b5c11dd1e6
Ethan Rose <erose@apache.org>
2021-08-21 16:27:25 -0700

    Clean up tmux config locations and keybindings
    Remove alacritty tmux keybindings that did not use the tmux prefix.

--- linux/tmux/.profile.d/99-kill-tmux.sh
+++ linux/tmux/.profile.d/99-kill-tmux.sh
@@ -3,6 +3,6 @@
 # Start with a fresh set of tmux sessions on each login for Linux.
 # Won't work on MacOS since every shell is login, also not needed since
 # sessions are killed on logout on MacOS.
-if command -v tmux > /dev/null && [ "$(uname -s)" != 'Darwin' ]; then
+if command -v tmux > /dev/null; then
     tmux kill-server
 fi
