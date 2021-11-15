0405cfd9585ec2052d351587588993b4c21d1fd5
Ethan Rose <erose@apache.org>
2021-11-15 09:18:48 -0800

    Fix accidental fork bomb oops

--- macos/sketchybar/.startup/yabai/yabai.d/sketchybar.sh
+++ macos/sketchybar/.startup/yabai/yabai.d/sketchybar.sh
@@ -10,8 +10,4 @@ yabai -m signal --add event=mission_control_check_for_exit action=sketchybar-yab
 # Refresh bar when window counts per space could have changed.
 yabai -m signal --add event=window_created action=sketchybar-yabai-reload label=sketchybar-windw-created
 yabai -m signal --add event=window_destroyed action=sketchybar-yabai-reload label=sketchybar-windw-destroyed
-# Handles case when window is moved using `yabai -m window --space <space>`
-# which does not trigger window_moved event.
-# Also handles space label change events.
-yabai -m signal --add event=daemon_message action=sketchybar-yabai-reload label=sketchybar-yabai-messaged
 
