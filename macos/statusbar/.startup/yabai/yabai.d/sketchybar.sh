6c69390d7e237ca66e782e5eb9575783f35ae32f
Ethan Rose <erose@apache.org>
2022-04-05 18:42:15 -0700

    [macos] more status bar updates

--- macos/statusbar/.startup/yabai/yabai.d/sketchybar.sh
+++ macos/statusbar/.startup/yabai/yabai.d/sketchybar.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Make space for the bar.
-sketchybar-yabai-set-position top
+position='top'
+sketchybar-yabai-set-position "$position"
 
 # Refresh bar when mission control is entered/exited.
 yabai -m signal --add event=mission_control_enter action='sketchybar-item-yabai-spaces --bar position=bottom'  label=sketchybar-mc-enter
@@ -13,6 +14,6 @@ yabai -m signal --add event=window_destroyed action='sketchybar-item-yabai-space
 yabai -m signal --add event=window_minimized action='sketchybar-item-yabai-spaces' label=sketchybar-windw-minimized
 yabai -m signal --add event=window_deminimized action='sketchybar-item-yabai-spaces' label=sketchybar-windw-deminimized
 # Full bar reload when displays are changed in case a notched display was added/removed.
-yabai -m signal --add event=display_added action='sketchybar-yabai-set-height' label=sketchybar-display-added
-yabai -m signal --add event=display_removed action='sketchybar-yabai-set-height' label=sketchybar-display-removed
+yabai -m signal --add event=display_added action="sketchybar-yabai-set-position $(yabai-get-bar-position)" label=sketchybar-display-added
+yabai -m signal --add event=display_removed action="sketchybar-yabai-set-position $(yabai-get-bar-position)" label=sketchybar-display-removed
 
