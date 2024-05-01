71141fec4102b661fb477a6f9a81b484065798ae
Ethan Rose <erose@apache.org>
2024-04-30 22:17:21 -0700

    Remove fzf launcher for macos

--- macos/fzf/.startup/profile/profile.d/50-python.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Macos by default will only have a python3 executable for system python.
-# m cli requires the executable to be naemd python, so add homebrew's symlink to the path.
-brew_python_dir="$(brew --prefix)/opt/python/libexec/bin/"
-if [ -d "$brew_python_dir" ]; then
-    PATH="$brew_python_dir:$PATH"
-fi
-
